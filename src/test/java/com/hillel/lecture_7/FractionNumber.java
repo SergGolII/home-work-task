5f35f4bba6ad9f31bfe9a4847e6a7ecbe8e8e006
SergGolII <serhii.holubiev.sh@gmail.com>
2019-11-12 08:15:45 +0200

    Update 4

--- src/test/java/com/hillel/lecture_7/FractionNumber.java
+++ src/test/java/com/hillel/lecture_7/FractionNumber.java
@@ -5,5 +5,93 @@ package com.hillel.lecture_7;
  */
 public class FractionNumber {
 
+    private double firstNumber = 0d;
+    private double secondNumber = 0d;
 
+    public FractionNumber(){ }
+
+    public FractionNumber(double firstNumber, double secondNumber){
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+    }
+
+    public double plus(double firstNumber, double secondNumber){
+        double result = firstNumber + secondNumber;
+        return result;
+    }
+
+    public double plus(){
+        double result = firstNumber + secondNumber;
+        return result;
+    }
+
+
+    public double minus(double firstNumber, double secondNumber){
+        double result = firstNumber - secondNumber;
+        return result;
+    }
+
+    public double minus(){
+        double result = firstNumber - secondNumber;
+        return result;
+    }
+
+
+    public double multiplication(double firstNumber, double secondNumber){
+        double result = firstNumber * secondNumber;
+        return result;
+    }
+
+    public double multiplication(){
+        double result = firstNumber * secondNumber;
+        return result;
+    }
+
+
+    public double division(double firstNumber, double secondNumber){
+        double result = 0;
+        if (secondNumber == 0){
+            System.out.println("\n" +
+                    "A test where the first number "+firstNumber+" is divided by the second number "+secondNumber+" is incorrect, since division by zero occurs!");
+        } else {
+            result = firstNumber / secondNumber;
+        }
+        return result;
+    }
+
+    public double division(){
+        double result = 0;
+        if (secondNumber == 0){
+            System.out.println("\n" +
+                    "A test where the first number "+firstNumber+" is divided by the second number "+secondNumber+" is incorrect, since division by zero occurs!");
+        } else {
+            result = firstNumber / secondNumber;
+        }
+        return result;
+    }
+
+
+    public double getFirstNumber() {
+        return firstNumber;
+    }
+
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public double getSecondNumber() {
+        return secondNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "FractionNumber{" +
+                "firstNumber=" + firstNumber +
+                ", secondNumber=" + secondNumber +
+                '}';
+    }
 }
--- src/test/java/com/hillel/lecture_7/FractionNumbersTestTask.java
+++ src/test/java/com/hillel/lecture_7/FractionNumbersTestTask.java
@@ -1,5 +1,8 @@
 package com.hillel.lecture_7;
 
+import org.testng.annotations.Test;
+import org.testng.annotations.DataProvider;
+import static org.testng.Assert.assertEquals;
 /**
  * Write class to represent FractionNumbers
  * This class should contain two fields.
@@ -10,9 +13,92 @@ package com.hillel.lecture_7;
  */
 public class FractionNumbersTestTask {
 
+    private FractionNumber fractionNumber = new FractionNumber();
+    private FractionNumber fractionNumberWithParam = new FractionNumber(75.0d, 50.0d);
 
+    @Test (dataProvider = "numbersProviderPlus")
+    public void plusTest(double firstNumber, double secondNumber, double expectedResult) {
+        assertEquals(fractionNumber.plus(firstNumber, secondNumber), expectedResult);
+    }
 
+    @DataProvider(name = "numbersProviderPlus")
+    public static Object[][] numbersProviderPlus() {
+        return new Object[][] {
+                { 20.0d, 10.0d, 30.0d},
+                { 50.0d, -30.0d, 20.0d},
+                { 25.0d, 0.0d, 25.0d},
+                { 0.0d, -30.0d, -30.0d}
+        };
+    }
 
+    @Test (dataProvider = "numbersProviderMinus")
+    public void minusTest(double firstNumber, double secondNumber, double expectedResult) {
+        assertEquals(fractionNumber.minus(firstNumber, secondNumber), expectedResult);
+    }
 
+    @DataProvider(name = "numbersProviderMinus")
+    public static Object[][] numbersProviderMinus() {
+        return new Object[][] {
+                { 20.0d, 10.0d, 10.0d},
+                { 50.0d, -30.0d, 80.0d},
+                { 25.0d, 0.0d, 25.0d},
+                { 0.0d, -30.0d, 30.0d}
+        };
+    }
+
+    @Test (dataProvider = "numbersProviderMultiplication")
+    public void multiplicationTest(double firstNumber, double secondNumber, double expectedResult) {
+        assertEquals(fractionNumber.multiplication(firstNumber, secondNumber), expectedResult);
+    }
+
+    @DataProvider(name = "numbersProviderMultiplication")
+    public static Object[][] numbersProviderMultiplication() {
+        return new Object[][] {
+                { 20.0d, 10.0d, 200.0d},
+                { 50.0d, -30.0d, -1500.0d},
+                { 25.0d, 0.0d, 0.0d},
+                { 0.0d, -30.0d, -0.0d}
+        };
+    }
+
+    @Test (dataProvider = "numbersProviderDivision")
+    public void divisionTest(double firstNumber, double secondNumber, double expectedResult) {
+        assertEquals(fractionNumber.division(firstNumber, secondNumber), expectedResult);
+    }
+
+    @DataProvider(name = "numbersProviderDivision")
+    public static Object[][] numbersProviderDivision() {
+        return new Object[][] {
+                { 20.0d, 10.0d, 2.0d},
+                { 50.0d, -25.0d, -2.0d},
+                { 25.0d, 0.0d, 0.0d},
+                { 0.0d, -30.0d, -0.0d}
+        };
+    }
+
+    @Test
+    public void withParamPlus() {
+        double expectedPlus = 125.0d;
+        assertEquals(fractionNumberWithParam.plus(), expectedPlus);
+    }
+
+    @Test
+    public void withParamMinus() {
+        double expectedMinus = 25.0d;
+        assertEquals(fractionNumberWithParam.minus(), expectedMinus);
+    }
+
+    @Test
+    public void withParamMultiplication() {
+        double expectedMultiplication = 3750.0d;
+        assertEquals(fractionNumberWithParam.multiplication(), expectedMultiplication);
+    }
+
+    @Test
+    public void withParamDivision() {
+        double expectedDivision = 1.5d;
+        assertEquals(fractionNumberWithParam.division(), expectedDivision);
+    }
 
 }
+
